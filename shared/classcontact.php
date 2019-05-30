9feb891a290e95ee0c0d074aa7db6ccdb5a4ec4e
parthshah05 <parth.babu05@gmail.com>
2019-05-30 23:50:04 +0530

    our template

---
+++ shared/classcontact.php
@@ -0,0 +1,35 @@
+
+<?php
+class contact
+{
+    private static $conn=null;
+    public static function  connect()
+    {
+        self::$conn=mysqli_connect("localhost","root","","priya_store");
+        return self::$conn;
+    }
+    public static function disconnect()
+    {
+        mysqli_close($conn);
+        self::$conn=null;
+    }
+    
+    
+    
+    public function insert($wname,$wemail,$wsub,$wmsg)
+    {
+        $cnn=contact::connect();
+        
+        $q="insert into contact_us values ('". $wname."','". $wemail ."','". $wsub ."','". $wmsg ."')";
+        
+        $result=$cnn->query($q);
+        return $result;
+        contact::disconnect();
+
+    }
+    
+    
+    
+
+}
+?>
